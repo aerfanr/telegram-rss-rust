474adf374d7318c899393576e2c06b40954997d7
Amirerfan Rafati <aerfanr@mailo.com>
2022-03-16 12:40:54 +0330

    Implemented config reader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use teloxide::prelude2::*;
 use teloxide::utils::command::BotCommand;
+use serde::Deserialize;
+use std::fs::File;
+use std::io::BufReader;
 use std::error::Error;
 
 #[derive(BotCommand, Clone)]
@@ -7,6 +10,31 @@ use std::error::Error;
 enum Command {
     Help,
     News
+}
+
+// Bot config structure
+#[derive(Deserialize, Debug)]
+struct Config {
+    sites: Vec<Site>
+}
+
+#[derive(Deserialize, Debug)]
+struct Site {
+    id: String,
+    url: String
+}
+
+// Read the config and store in CONFIG for global access
+thread_local!(static CONFIG: Config = match get_config() {
+    Ok(c) => c,
+    Err(e) => panic!("{}", e)
+});
+
+// Read config from file
+fn get_config() -> Result<Config, Box<dyn Error>> {
+    let file = File::open("/opt/rss.json")?;
+    let reader = BufReader::new(file);
+    Ok(serde_json::from_reader(reader)?)
 }
 
 // Generate a news message
@@ -39,6 +67,11 @@ async fn answer(bot: AutoSend<Bot>, message: Message, command: Command)
 async fn main() {
     teloxide::enable_logging!();
     log::info!("Starting...");
+
+    // log bot config
+    CONFIG.with(|config| {
+        log::debug!("Config: {:#?}", config)
+    });
 
     let bot = Bot::from_env().auto_send();
 
